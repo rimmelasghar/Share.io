d9fb68f143e04c60465bf4cd89bfba9e807a165f
rimmelasghar <rimmelasghar3@gmail.com>
2022-09-18 23:47:16 +0500

    updated1

--- shareapp/views.py
+++ shareapp/views.py
@@ -26,13 +26,11 @@ def sucess(request):
 def download_file(request,key):
     # fill these variables with real values
     files = Files.objects.get(key = key)
-    # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     filename = str(files.file)
-    fl_path = 'F:/Programming/Django/github/shareio/media/'+filename
+    fl_path = BASE_DIR+'/media/'+filename
     fl = open(fl_path, 'rb')
     mime_type, _ = mimetypes.guess_type(filename)
     response = HttpResponse(fl, content_type=mime_type)
     response['Content-Disposition'] = "attachment; filename=%s" % filename
     return response
-
-    
